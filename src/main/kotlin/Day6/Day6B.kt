8c1916a5c6c000a71cf51ad81e9b4163f85e9044
Christoffer <links685@gmail.com>
2021-12-07 16:49:25 +0100

    did day 6B

---
+++ src/main/kotlin/Day6/Day6B.kt
@@ -0,0 +1,23 @@
+package Day6
+
+import java.io.File
+
+fun main() {
+    solve()
+}
+
+
+fun solve() {
+    val input = File("input6.txt").readText()
+        .split(",").map { s -> s.toLong() }
+    val arr = mutableListOf(0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L)
+    for (i in input) {
+        arr[i.toInt()]++
+    }
+    println(arr)
+    for (i in 0 until 256) {
+        arr.add(arr.removeAt(0))
+        arr[6] += arr[8]
+    }
+    println(arr.sum())
+}
